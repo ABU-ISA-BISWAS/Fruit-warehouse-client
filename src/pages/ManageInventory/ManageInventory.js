631d4d2e3602fadbe4b031814ac013d9e35dc1e1
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-05-05 00:28:53 +0600

    manage inventory

--- src/pages/ManageInventory/ManageInventory.js
+++ src/pages/ManageInventory/ManageInventory.js
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 const ManageInventory = () => {
     const [fruits, setFruits] = useState([]);
+    
 
     useEffect(() => {
         fetch('http://localhost:5000/fruit')
             .then(res => res.json())
             .then(data => setFruits(data))
     }, []);
+
+    const handleDelete = id=>{
+        const proceed = window.confirm('Are you sure?');
+        if(proceed){
+            const url = `http://localhost:5000/fruit/${id}`;
+            fetch(url,{
+                method:'DELETE',
+               
+            })
+            
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.deletedCount > 0){
+                    console.log(data);
+                const remaining =fruits.filter(fruit=>fruit._id !==id);
+                setFruits(remaining);
+                }
+                
+            })
+        }
+    }
 
     return (
         <div>
@@ -27,7 +50,17 @@ const ManageInventory = () => {
         <td>{fruit.name}</td>
         <td>{fruit.price}</td>
         <td>{fruit.quantity}</td>
-        <td><button className='btn btn-success'>Update</button> <button className='btn btn-danger'>Delete</button></td>
+        <td>
+       
+                <div key={fruit._id} >
+                <button   className='btn btn-danger me-2' onClick={()=>handleDelete(fruit._id)}>Delete</button>
+                   <button className='btn btn-success'>Update</button>
+                </div>
+                   
+               
+            
+        </td>
+        
       </tr>)
  }
     
